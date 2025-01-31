2f7efb291d3877ed4493949433d0edea6956c32f
phte1100 <phte1100@student.miun.se>
2025-01-31 14:22:32 +0100

    Fixat med controller och ordnat med design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Moment2Mvc.Models;
 
 namespace Moment2Mvc.Controllers;
 
@@ -9,14 +11,39 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpGet("/planering")]
-    public IActionResult planing()
-    {
+    [HttpPost] 
+    public IActionResult Index(FishModel Model) {
         return View();
     }
+
+    [HttpGet("/log")]
+    public IActionResult Log(FishModel model)
+    {
+        //Validera input
+        if(ModelState.IsValid) 
+        {
+            // Läsa in json-fil
+            string jsonStr = System.IO.File.ReadAllText("fiskar.json");
+            // deserialisera json-fil
+            var fishList = JsonSerializer.Deserialize<List<FishModel>>(jsonStr);
+
+            // Lägg till ny fisk
+            if (fishList != null) 
+            {
+                fishList.Add(model);
+                // Serialisera listan
+                jsonStr = JsonSerializer.Serialize(fishList);
+                System.IO.File.WriteAllText("fiskar.json", jsonStr);
+            }
+        }
+        return View();
+    }
+
     [HttpGet("/omsidan")]
     public IActionResult About()
     {
         return View();
     }
+    
+
 }
---
+++ Models/FishModel.cs
@@ -0,0 +1,20 @@
+namespace Moment2Mvc.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class FishModel
+    {
+        [Required(ErrorMessage = "Fiskesort är obligatoriskt")]
+        public required string Species { get; set; }
+        [Required(ErrorMessage = "Plats är obligatoriskt")]        
+        public required string Location { get; set; }
+        [Range(1, 100000), Required (ErrorMessage = "Vikt måste vara mellan 1 och 100000 gram")]
+        public int Weight { get; set; }
+        [Range(1, 300), Required (ErrorMessage = "Längd måste vara mellan 1 och 300 cm")]
+        public int Length { get; set; }
+        [Required(ErrorMessage = "Datum är obligatoriskt")]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Beskrivning är obligatoriskt,")]
+        [MaxLength(250, ErrorMessage = "Beskrivning får inte vara längre än 250 tecken")]
+        public required string Description { get; set; }
+    }
+}
